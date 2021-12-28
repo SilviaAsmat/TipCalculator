c8ae3eed3135a9c893a773b2c05bfb565a5f7cd9
Silvia Asmat <silvia.asmat@g.austincc.edu>
2021-12-28 12:19:30 -0600

    #4: Added TextWatcher, billTotalListener

--- app/src/main/java/com/silviaasmat/tipcalculator/MainActivity.java
+++ app/src/main/java/com/silviaasmat/tipcalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.silviaasmat.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        observeBillTotal();
+    }
+
+    private void observeBillTotal() {
+        EditText input = findViewById(R.id.billTotalInput);
+        input.addTextChangedListener(new BillTotalListener());
+    }
+
+    class BillTotalListener implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String currentValue = editable.toString();
+            Log.v("SAA", currentValue);
+        }
     }
 }
